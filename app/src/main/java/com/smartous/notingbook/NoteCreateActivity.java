b8deebd9359d6fcb11c325784de6e0115ad4bb50
SFocus <svyatoslav472@gmail.com>
2017-02-28 12:48:11 +0200

    Note create activity

---
+++ app/src/main/java/com/smartous/notingbook/NoteCreateActivity.java
@@ -0,0 +1,15 @@
+package com.smartous.notingbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.labs.notingbook.R;
+
+public class NoteCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_create);
+    }
+}
