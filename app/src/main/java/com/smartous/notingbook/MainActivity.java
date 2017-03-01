daf8ac3a0aec1fadf91eeaff9fbb0e2e91c57b18
Drummer01 <self1995@mail.ua>
2017-03-01 16:58:11 +0200

    Capability edit notes

--- app/src/main/java/com/smartous/notingbook/MainActivity.java
+++ app/src/main/java/com/smartous/notingbook/MainActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.labs.notingbook.R;
 import com.labs.notingbook.noting.NoteModel;
@@ -15,7 +19,8 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int CREATE_NEW_NOTE_ACTION = 100;
+    public static final int ACTION_CREATE_NOTE = 100;
+    public static final int ACTION_EDIT_NOTE = 101;
 
     private RecyclerView mNotesRecycler;
     private ArrayList<NoteModel> mNotes = new ArrayList<>();
@@ -32,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 .withDate(new Date())
                 .withImportanceLevel(NoteModel.ImportanceLevel.One)
                 .build();
-       // mNotes.add(nm);
+        //mNotes.add(nm);
+        //mNotes.add(nm);
 
 
         mNotesRecycler = (RecyclerView) findViewById(R.id.recycler_notes);
+        registerForContextMenu(mNotesRecycler);
+
         notesAdapter = new NotesAdapter(mNotes);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -44,16 +52,50 @@ public class MainActivity extends AppCompatActivity {
         mNotesRecycler.setLayoutManager(llm);
         mNotesRecycler.setAdapter(notesAdapter);
 
+
         Intent createNewNoteIntent = new Intent(this, NoteCreateActivity.class);
-        startActivityForResult(createNewNoteIntent, CREATE_NEW_NOTE_ACTION);
+        startActivityForResult(createNewNoteIntent, ACTION_CREATE_NOTE);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.setHeaderTitle(R.string.option_menu_header);
+        MenuInflater menuInflater = this.getMenuInflater();
+        menuInflater.inflate(R.menu.cardview_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_menu_edit : {
+                Intent editIntent = new Intent(this, NoteCreateActivity.class);
+                editIntent.putExtra(NoteCreateActivity.KEY_NOTE_MODEL, mNotes.get(notesAdapter.getPosition()));
+                editIntent.putExtra(NoteCreateActivity.KEY_NOTE_INDEX, notesAdapter.getPosition());
+                editIntent.setAction(Intent.ACTION_EDIT);
+                startActivityForResult(editIntent, ACTION_EDIT_NOTE);
+                break;
+            }
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             switch (requestCode) {
-                case CREATE_NEW_NOTE_ACTION : {
+                case ACTION_CREATE_NOTE : {
                     NoteModel note = data.getParcelableExtra(NoteCreateActivity.NEW_NOTE);
                     mNotes.add(note);
+                    notesAdapter.notifyDataSetChanged();
+                    break;
+                }
+
+                case ACTION_EDIT_NOTE : {
+                    int index = data.getIntExtra(NoteCreateActivity.KEY_NOTE_INDEX, -1);
+                    NoteModel note = data.getParcelableExtra(NoteCreateActivity.KEY_NOTE_MODEL);
+                    mNotes.set(index, note);
                     notesAdapter.notifyDataSetChanged();
                     break;
                 }
